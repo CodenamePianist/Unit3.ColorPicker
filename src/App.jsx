bab77a72992bca3d95ccef7d1f9dd76b19ede3f8
Brandon Hamilton <bham2015@gmail.com>
2024-09-24 22:07:00 -0500

    The changes in this commit will add the functionality when a color is clicked.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-// Write your Color component here
-const Color = (props) => {
-  return <div className="yellow"></div>
+import { useState } from "react";
+
+
+const Color = ({ color, setSelectedColor }) => {
+  return <div className={color} onClick={() => setSelectedColor(color)}></div>
 };
 
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState("");
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
       <div id="colors-list">
-        {/* colors go here */}
-        <Color color="red"/>
-        <Color color="black"/>
-        <Color color="violet"/>
+        <Color color="red" setSelectedColor={setSelectedColor}/>
+        <Color color="black" setSelectedColor={setSelectedColor}/>
+        <Color color="violet" setSelectedColor={setSelectedColor}/>
       </div>
     </div>
   );
